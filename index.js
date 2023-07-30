0baddfc7158d232037068e8dced5d13fa162b955
Cara Qing Ge <geqing2012@hotmail.com>
2023-07-30 14:18:03 -0400

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from 'mongoose';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"));
